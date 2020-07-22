5b9f6b186499c8267739fd51df55093d4d926eb4
neutercrux <yash.r.patil98@gmail.com>
2020-07-22 20:41:46 +0530

    added mongodb uri

--- CollaborativeClassroomBackend/models/Model.js
+++ CollaborativeClassroomBackend/models/Model.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://127.0.0.1:27017/user')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/user')
 
 var UserSchema = new mongoose.Schema({
     usn : {
